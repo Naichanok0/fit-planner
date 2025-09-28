e8d7a48c736535f361aad46ea2361d5758982db4
DESKTOP-6ORS5VR\Admin <pansan.c@ku.th>
2025-09-28 23:41:17 +0700

    no message

--- Project/components/themed-view.tsx
+++
@@ -1,9 +0,0 @@
-import { View, StyleSheet } from 'react-native';
-
-export function ThemedView({ children, style }: any) {
-  return <View style={[styles.default, style]}>{children}</View>;
-}
-
-const styles = StyleSheet.create({
-  default: { backgroundColor: '#fff' },
-});
